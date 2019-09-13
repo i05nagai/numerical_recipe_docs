081c4d95aee5b9f06467687f05f5cf4020863d74
i05nagai <i05nagai@gmail.com>
2019-09-14 05:13:47 +0900

    Add generated files

---
+++ _static/js/config.mathjax.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function(_doc, _win) {
+  window.MathJax = {
+    tex2jax: { 
+        inlineMath: [['$','$']],
+        displayMath: [['$$', '$$']],
+    },
+    TeX: { 
+      equationNumbers: { 
+        autoNumber: "AMS",
+        formatNumber: function (n) {
+          return n;
+        }
+      },
+      Macros: {
+        diag: '\\mathop{\\mathrm{diag}}\\nolimits',
+        argmax: '\\mathop{\\mathrm{argmax}}\\limits',
+        argmin: '\\mathop{\\mathrm{argmin}}\\limits',
+        deg: '\\mathrm{deg}',
+        norm: ['\\left\\| #1 \\right\\|', 1],
+        abs: ['\\left| #1 \\right|', 1],
+      }
+    }
+  };
+})(document, window);
+
